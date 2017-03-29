4b2ee4ec7e4e1b2a1d835cb1a82c970dbb491eb4
stephenhu3 <stephenhu3@gmail.com>
2017-03-28 20:53:15 -0700

    added analytics to template pages too

--- templates/notes.php
+++ templates/notes.php
@@ -9,6 +9,15 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js" type="text/javascript"></script>
     <script src="../js/notetake.js"></script>
     <script src="../js/bootstrap-confirm-button.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-57297383-1', 'auto');
+      ga('send', 'pageview');
+    </script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" type="text/css">
     <link rel="stylesheet" href="../css/styles.css" type="text/css">
 </head>
